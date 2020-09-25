4eb63593eb43059da4cc61c6402ffd2f5e209602
Chaitanya-shikhare <59410651+Chaitanya-shikhare@users.noreply.github.com>
2020-09-25 20:07:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const pepe = ['pepe'];
+
+function findPepe(array) {
+  for (let i=0; i< array.length; i++) {
+    if (array[i] === "pepe") {
+      console.log('Found PEPE');
+    }
+  }
+}
+
+findPepe(pepe);
